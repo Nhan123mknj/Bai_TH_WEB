987d7d680eaee8767da2772f5bb9d4912df46cd3
Nhan123mknj <buidanhnhan0693@gmail.com>
2023-10-02 08:16:33 +0700

    init project

---
+++ startup/Components/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using startup.Models;
+namespace startup.Components
+
+{
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent:ViewComponent
+    {
+        private DataContext _Context;
+        public MenuViewComponent(DataContext context)
+        {
+            _Context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofmenu =(from m in _Context.Menu
+                             where(m.IsActive==true)&&(m.Position==1)
+                             select m).ToList();
+            return await Task.FromResult((IViewComponentResult)View("Default",listofmenu));
+        }
+    }
+}
---
+++ startup/Models/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace startup.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        { }
+        public DbSet<Menu> Menu { get; set; }
+    }
+}
